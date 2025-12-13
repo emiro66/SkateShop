9ecb7142d1f3383cc0ed2f7def11c4d82491c451
emiro66 <hccvhd566@gmail.com>
2025-12-13 17:44:33 +0700

    ...

--- src/components/pages/CarouselPage.vue
+++ src/components/pages/CarouselPage.vue
@@ -3,24 +3,53 @@ import slides from '@/objects/slides.ts'
 </script>
 
 <template>
-    <v-carousel class="carousel-slides">
-      <v-carousel-item :src="slide.image" cover v-for="slide in slides">
-        <p class="text-slide text-center">{{ slide.text}}</p>
-        <v-btn class="button-slide " v-if="slide.button !=='' " >  {{ slide.button}}</v-btn>
+  <v-carousel class="carousel-slides">
+    <v-carousel-item :src="slide.image" cover v-for="slide in slides">
+      <v-container class="ma-0 slide-container">
+        <p class="text-slide text-center">{{ slide.text }}</p>
+        <v-container class="button-slide">
+          {{ slide.button.text }}
+        </v-container>
+      </v-container>
 
-      </v-carousel-item>
-    </v-carousel>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <style>
-  .button-slide button{
-    margin: auto;
-  }
+
+.v-carousel-item :deep(.v-responsive__content){
+   min-height: 100vh;
+}
+.v-responsive__content{
+  min-height: 100vh;
+}
+
+.carousel-slides {
+   min-height: 100vh;
+}
+
+.v-carousel-item {
+  min-height: 100vh;
+}
+
+.slide-container {
+  min-width: 100%;
+}
+
+.button-slide:hover {
+  background-color: rgb(255, 231, 199);
+}
+
+.button-slide {
+  background-color: antiquewhite;
+  width: fit-content;
+  border-radius: 20px;
+}
 
 
 .text-slide {
   font-size: 50px;
   margin-top: 50px;
 }
-
 </style>
